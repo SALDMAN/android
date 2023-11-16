d59f07a36a72e7cad858d75967735433cfbc77e7
SALDMAN <109432390+SALDMAN@users.noreply.github.com>
2023-11-16 09:27:21 +0200

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -2,37 +2,44 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TTSManager ttsManager=null;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TTSManager ttsManager = null;
     private EditText et;
     private Button bt;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ttsManager = new TTSManager(this);
-        et=findViewById(R.id.et);
-        bt=findViewById(R.id.bt);
-        bt.setOnClickListener(this::speakOnClick);
+
+        et = findViewById(R.id.et);
+        bt = findViewById(R.id.bt);
+        bt.setOnClickListener(this);
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         ttsManager.shutdown();
     }
-    public void speakOnClick(View v) {
 
+    public void speakOnClick() {
         String text = et.getText().toString();
-        ttsManager.speak(text,true);
+        Log.d("MainActivity", "Text to speak: " + text);
+        ttsManager.speak(text, true);
     }
 
+
+    @Override
+    public void onClick(View view) {
+        speakOnClick();
+    }
 }
