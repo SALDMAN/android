0b8065de38fc36cee3c067b12af421c0fc41d47c
SALDMAN <109432390+SALDMAN@users.noreply.github.com>
2023-12-05 15:04:11 +0200

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,114 +1,84 @@
-package com.example.anew;
+package com.example.myapplication;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.PopupMenu;
-import androidx.annotation.NonNull;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
-    private int selectedBgColor = Color.BLACK;
-    private int selectedTextColor = Color.WHITE;
-    private int selectedImageResource = 0;
-    private Button btnBackgroundColor;
-    private Button btnForegroundColor;
-    private Button btnChooseImage;
-    private EditText editTextGreeting;
-    private Button btnNext;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button bt1, bt2, bt3, bt4, bt5, bt6;
+    private final CharSequence[] colors_items = {"Red", "Green", "Blue"}; //color choices
+    private AlertDialog exitdialog, colordialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnBackgroundColor = findViewById(R.id.btnBackgroundColor);
-        btnForegroundColor = findViewById(R.id.btnForegroundColor);
-        btnChooseImage = findViewById(R.id.btnChooseImage);
-        editTextGreeting = findViewById(R.id.editTextGreeting);
-        btnNext = findViewById(R.id.btnNext);
-
-        btnBackgroundColor.setOnClickListener(new View.OnClickListener() {
+        bt1=findViewById(R.id.bt1);
+        bt2 = findViewById(R.id.bt2);
+        bt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showPopupMenu(btnBackgroundColor, R.menu.menu2);
+                AlertDialog.Builder builder = new AlertDialog.Builder(this); //create alert dialog builder
+                builder.setTitle("Pick background color"); //set dialog title
+                builder.setMultiChoiceItems(colors_items, -1, this::onColorClick); //set color listener
+                builder.setCancelable(false); //make the dialog uncancelable
+                builder.setIcon(android.R.drawable.star_on); //set dialog icon
+                colordialog = builder.create(); //create dialog from builder
+                colordialog.show(); //show dialog
+
+            }
+                private void onColorClick (DialogInterface dialogInterface,int which){
+                }
+
+            private void listener(DialogInterface dialogInterface, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        finish();   //prevent returning here by using the back key
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
             }
         });
-
-        btnForegroundColor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showPopupMenu(btnForegroundColor, R.menu.menu3);
-            }
-        });
-
-        btnChooseImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showPopupMenu(btnChooseImage, R.menu.main_menu);
-            }
-        });
-
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String greetingText = editTextGreeting.getText().toString();
-
-                // Start SecondActivity with chosen options
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("bgColor", selectedBgColor);
-                intent.putExtra("textColor", selectedTextColor);
-                intent.putExtra("imageResource", selectedImageResource);
-                intent.putExtra("greetingText", greetingText);
-                startActivity(intent);
-            }
-        });
+        bt1.setOnClickListener(this);
     }
 
-    private void showPopupMenu(final Button button, final int menuRes) {
-        PopupMenu popupMenu = new PopupMenu(MainActivity.this, button);
-        popupMenu.getMenuInflater().inflate(menuRes, popupMenu.getMenu());
 
-        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(android.view.MenuItem menuItem) {
-                handleMenuItemClick(menuItem.getItemId());
-                return true;
-            }
-        });
-
-        popupMenu.show();
+    private void ExitConfirmDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this); // create alert dialog builder
+        builder.setTitle("Exit confirmation"); // set dialog title
+        builder.setMessage("Are you sure you want to exit ?"); // set dialog text
+        builder.setPositiveButton("Yes", this::onExitChosen); // set listener - if positive button is pressed
+        builder.setNegativeButton("No", this::onExitChosen); // set listener - if negative button is pressed
+        builder.setCancelable(false); // make the dialog uncancelable
+        builder.setIcon(android.R.drawable.ic_menu_close_clear_cancel); // set dialog icon
+        exitdialog = builder.create(); // create dialog from builder
+        exitdialog.show(); // show dialog
     }
 
-    private void handleMenuItemClick(int itemId) {
-        View v = getWindow().getDecorView(); // bg view
-        if (itemId == R.id.colorGreen) {
-            selectedBgColor = Color.parseColor("#00FF00"); // Green background
-            selectedTextColor = Color.parseColor("#000000"); // Black text
-        } else if (itemId == R.id.colorYellow) {
-            selectedBgColor = Color.parseColor("#FFFF00"); // Yellow background
-            selectedTextColor = Color.parseColor("#000000"); // Black text
-        } else if (itemId == R.id.colorBlue) {
-            selectedBgColor = Color.parseColor("#0000FF"); // Blue background
-            selectedTextColor = Color.parseColor("#FFFFFF"); // White text
-        } else if (itemId == R.id.menu_item1) {
-            selectedImageResource = R.drawable.hanuc;
-            selectedTextColor = Color.parseColor("#000000"); // Black text
-        } else if (itemId == R.id.menu_item2) {
-            selectedImageResource = R.drawable.safgi;
-            selectedTextColor = Color.parseColor("#FFFFFF"); // White text
-        } else if (itemId == R.id.menu_item3) {
-            selectedImageResource = R.drawable.sef;
-            selectedTextColor = Color.parseColor("#00FF00"); // Green text
+    public void onExitChosen(DialogInterface dialog, int which) {
+        switch (which) {
+            case DialogInterface.BUTTON_POSITIVE:
+                finish();   //prevent returning here by using the back key
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                break;
         }
     }
+
+    @Override
+    public void onClick(View view) {
+        ExitConfirmDialog();
     }
-
-
-
+}
